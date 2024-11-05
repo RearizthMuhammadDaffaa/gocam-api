c671ada9fcec3c541129aeb5c1b205f53291c9d2
Rearizth <astarothhellprince@gmail.com>
2024-11-05 14:57:31 +0700

    change database

--- config/Database.js
+++ config/Database.js
@@ -8,14 +8,9 @@ const db = new Sequelize(process.env.DB_NAME,
   process.env.DB_PASSWORD,
   {
   host:process.env.DB_HOST,
-  port:process.env.DB_PORT,
   dialect:'mysql',
   dialectModule:mysql2,
-  pool: {
-    max: 5,  // jumlah koneksi maksimum dalam pool
-    min: 0,
-    idle: 10000, // waktu dalam milidetik sebelum koneksi diambil dari pool
-  },
+ 
 })
 // const db = new Sequelize('rental_kamera','root','',{
 //   host:'localhost',
